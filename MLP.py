f669bdc8224dd20f6daa057f904e678905dc7af9
Omri Melcer <omelcer@gmail.com>
2026-01-07 15:04:21 +0200

    finished coding before results.

--- MLP.py
+++ MLP.py
@@ -12,21 +12,25 @@ class EuropeDataset(Dataset):
             csv_file (string): Path to the CSV file with annotations.
         """
         #### YOUR CODE HERE ####
+        try: 
+            df = pd.read_csv(csv_file)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"File {csv_file} not found.")
+        self. features = torch.from_pandas (df.drop('country', axis=1)).float()
+        self.labels = torch.from_pandas (df['country']).long()
         # Load the data into a tensors
         # The features shape is (n,d)
         # The labels shape is (n)
         # The feature dtype is float
         # THe labels dtype is long
-
         #### END OF YOUR CODE ####
-        pass
 
     def __len__(self):
         """Returns the total number of samples in the dataset."""
         #### YOUR CODE HERE ####
-        pass
+        return self.features.shape[0]
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx) :
         """
         Args:
             idx (int): Index of the data row
@@ -35,7 +39,7 @@ class EuropeDataset(Dataset):
             dictionary or list corresponding to a feature tensor and it's corresponding label tensor
         """
         #### YOUR CODE HERE ####
-        pass
+        return self.features[idx], self.labels[idx]
     
 
 class MLP(nn.Module):
@@ -48,11 +52,19 @@ class MLP(nn.Module):
             output_dim (int): The output dimension, should match the number of classes in the dataset
         """
         #### YOUR CODE HERE ####
-        pass
+        self.hiddem_layer = []
+        input_dim = 2  # since we have longitude and latitude as input features
+        for _ in range(num_hidden_layers):
+            self.hiddem_layer.append(nn.Linear(input_dim, hidden_dim))
+            self.hiddem_layer.append(nn.BatchNorm1d(hidden_dim))
+            self.hiddem_layer.append(nn.ReLU())
+            input_dim = hidden_dim
+        self.hiddem_layer.append(nn.Linear(hidden_dim, output_dim))
+        self.model = nn.Sequential(*self.hiddem_layer)
 
     def forward(self, x):
         #### YOUR CODE HERE ####
-       pass
+       return self.model(x)
 
 
 def train(train_dataset, val_dataset, test_dataset, model, lr=0.001, epochs=50, batch_size=256):    
@@ -63,8 +75,8 @@ def train(train_dataset, val_dataset, test_dataset, model, lr=0.001, epochs=50, 
     
     #### YOUR CODE HERE ####
     # initialize your criterion and optimizer here
-    # criterion = 
-    # optimizer = 
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
     train_accs = []
     val_accs = []
@@ -72,33 +84,64 @@ def train(train_dataset, val_dataset, test_dataset, model, lr=0.001, epochs=50, 
     train_losses = []
     val_losses = []
     test_losses = []
-
+    best_val_acc = 0.0
+    best_loss_val = float('inf')
+    best_model = None
+    ep_where_best = 0
     for ep in range(epochs):
         #
         model.train()
-        #### YOUR CODE HERE ####
-        # perform training epoch here
-
-
+        for batch_X, batch_y in trainloader:
+            #### YOUR CODE HERE ####
+            # perform training loop here
+            optimizer.zero_grad()
+            predictions  = model(batch_X)
+            loss = criterion(predictions, batch_y)
+            loss.backward()
+            optimizer.step()
         model.eval()
         with torch.no_grad():
             #### YOUR CODE HERE ####
             # perform validation loop and test loop here
-            pass
-
+            train_loss, train_acc = measure_current_model_on_set(trainloader, model, criterion)
+            val_loss, val_acc = measure_current_model_on_set(valloader, model, criterion)
+            test_loss, test_acc = measure_current_model_on_set(testloader, model, criterion)
+            train_losses.append(train_loss)
+            val_losses.append(val_loss)
+            test_losses.append(test_loss)
+            train_accs.append(train_acc)
+            val_accs.append(val_acc)
+            test_accs.append(test_acc)
+        if val_loss<best_loss_val:
+            best_loss_val = val_loss
+            best_model = model.state_dict()
+            ep_where_best = ep
         print('Epoch {:}, Train Acc: {:.3f}, Val Acc: {:.3f}, Test Acc: {:.3f}'.format(ep, train_accs[-1], val_accs[-1], test_accs[-1]))        
+    return model, train_accs, val_accs, test_accs, train_losses, val_losses, test_losses 
 
-    model, train_accs, val_accs, test_accs, train_losses, val_losses, test_losses 
-
-
+def measure_current_model_on_set(dataloader, model, criterion):
+    # implement a function that measures accuracy and loss on a given dataloader and model
+    total_loss = 0.0
+    total_correct = 0.0
+    total_samples = 0.0
+    with torch.no_grad():
+        for batch_X, batch_y in dataloader:
+            predictions = model(batch_X)
+            total_loss += criterion(predictions, batch_y).item() * batch_X.size(0)
+            total_correct += (predictions.argmax(dim=1) == batch_y).sum().item()
+            total_samples += batch_X.size(0)
+    avg_loss = total_loss / total_samples
+    accuracy = total_correct / total_samples
+    return avg_loss, accuracy
+    #### END OF YOUR CODE ####
 
 if __name__ == '__main__':
     # seed for reproducibility
     torch.manual_seed(0)    
 
-    train_dataset = EuropeDataset('train.csv')
-    val_dataset = EuropeDataset('validation.csv')
-    test_dataset = EuropeDataset('test.csv')
+    train_dataset = EuropeDataset('input+data/train.csv')
+    val_dataset = EuropeDataset('input+data/validation.csv')
+    test_dataset = EuropeDataset('input+data/test.csv')
 
     #### YOUR CODE HERE #####
     # Find the number of classes, e.g.:
